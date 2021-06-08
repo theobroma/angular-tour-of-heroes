4abee6ab75a9ba32cc672b0d879730509bab0e2d
Alexandr Siryj <theobroma222@gmail.com>
2021-06-08 23:42:50 +0300

    deleteHero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -75,5 +75,15 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
 
